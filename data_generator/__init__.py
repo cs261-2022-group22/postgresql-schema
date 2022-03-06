68b2eefc633c16782c3863fab27b48567b426a04
Moody <mooodyhunter@outlook.com>
2022-03-06 02:06:52 +0000

    update data generator, supports Skills, MenteeSkills, MentorSkills

--- data_generator/__init__.py
+++ data_generator/__init__.py
@@ -1,6 +1,5 @@
-from datetime import date
+from datetime import date, datetime
 import sys
-import bcrypt
 
 
 def printe(*args, **kwargs):
@@ -56,12 +55,12 @@ class Mentee:
 class MentorMessage:
     def __init__(self, _id: int, _mentor: Mentor,  _message: str) -> None:
         self.id = _id
-        self.mentee = _mentor
+        self.mentor = _mentor
         self.message = _message
         pass
 
     def __str__(self) -> str:
-        return f"INSERT INTO MentorMessage VALUES({self.id}, {self.mentee.id}, '{self.message}');"
+        return f"INSERT INTO MentorMessage VALUES({self.id}, {self.mentor.id}, '{self.message}');"
 
 
 class MenteeMessage:
@@ -73,3 +72,70 @@ class MenteeMessage:
 
     def __str__(self) -> str:
         return f"INSERT INTO MenteeMessage VALUES({self.id}, {self.mentee.id}, '{self.message}');"
+
+
+class Skill:
+    def __init__(self, _id: int, _name: str) -> None:
+        self.id = _id
+        self.name = _name
+        pass
+
+    def __str__(self) -> str:
+        return f"INSERT INTO Skill VALUES({self.id}, '{self.name}');"
+
+
+class Workshop:
+    def __init__(self, _id: int, _skill: Skill, _start: datetime, _duration_minutes: int, _link: str) -> None:
+        self.id = _id
+        self.skill = _skill
+        self.start = _start
+        self.duration_minutes = _duration_minutes
+        self.link = _link
+        pass
+
+    def __str__(self) -> str:
+        return f"INSERT INTO Workshop VALUES({self.id}, {self.skill.id}, '{self.link}', {self.start}, {self.duration_minutes});"
+
+
+class Assignment:
+    def __init__(self, _id: int, _mentor: Mentor, _mentee: Mentee) -> None:
+        self.id = _id
+        self.mentor = _mentor
+        self.mentee = _mentee
+        pass
+
+    def __str__(self) -> str:
+        return f"INSERT INTO Assignment VALUES({self.id}, {self.mentor.id}, {self.mentee.id})"
+
+
+class Meeting:
+    def __init__(self, _id: int, _assignment: Assignment, _start: datetime, _duration_minutes: int, _link: str) -> None:
+        self.id = _id
+        self.assignment = _assignment
+        self.start = _start
+        self.duration_minutes = _duration_minutes
+        self.link = _link
+        pass
+
+    def __str__(self) -> str:
+        return f"INSERT INTO Meeting VALUES({self.id}, {self.assignment.id}, '{self.link}', {self.start}, {self.duration_minutes});"
+
+
+class MentorSkill:
+    def __init__(self, _id: int, _mentor: Mentor, _skill: Skill) -> None:
+        self.id = _id
+        self.mentor = _mentor
+        self.skill = _skill
+
+    def __str__(self) -> str:
+        return f"INSERT INTO MentorSkill VALUES({self.id}, {self.mentor.id}, {self.skill.id})"
+
+
+class MenteeSkill:
+    def __init__(self, _id: int, _mentee: Mentee, _skill: Skill) -> None:
+        self.id = _id
+        self.mentee = _mentee
+        self.skill = _skill
+
+    def __str__(self) -> str:
+        return f"INSERT INTO MenteeSkill VALUES({self.id}, {self.mentee.id}, {self.skill.id})"
