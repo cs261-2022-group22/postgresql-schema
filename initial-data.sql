10f3c5a73a401d079c4e79ef25dd5f07bc632709
Moody <mooodyhunter@outlook.com>
2022-03-12 14:45:35 +0000

    add initial-data.sql

---
+++ initial-data.sql
@@ -0,0 +1,13 @@
+-- Business Sectors
+INSERT INTO BusinessSector VALUES(DEFAULT, 'Private Bank');
+INSERT INTO BusinessSector VALUES(DEFAULT, 'Corporate Bank');
+INSERT INTO BusinessSector VALUES(DEFAULT, 'Asset Management');
+INSERT INTO BusinessSector VALUES(DEFAULT, 'Investment Bank');
+
+
+-- Skills
+INSERT INTO Skill VALUES(DEFAULT, 'Technical');
+INSERT INTO Skill VALUES(DEFAULT, 'How To Progress Career');
+INSERT INTO Skill VALUES(DEFAULT, 'Management');
+INSERT INTO Skill VALUES(DEFAULT, 'Leadership');
+INSERT INTO Skill VALUES(DEFAULT, 'Healthy Work-Life balance');
